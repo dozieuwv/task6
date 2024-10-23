f6bd67041b45172d10c9aa31c504a6c4626688fb
dozieuwv <atugwuchiedozieprivate@gmail.com>
2024-10-23 09:53:53 +0100

    first commit

---
+++ task66.js
@@ -0,0 +1,21 @@
+let x = ( 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 ,10 );
+  
+    const add = (x) <=60;
+    
+console.log(x);
+
+    
+    for (let x = (2 ,3 , 4 , 5 , 6 , 7 , 8 , 9 ,10); x <= 60; x++) {
+        
+         if (x===0) {
+            const result =x*2;
+            console.log(x);
+
+            
+         } else {
+            const result =x-2;
+            console.log(x);
+            
+         }
+
+    }
